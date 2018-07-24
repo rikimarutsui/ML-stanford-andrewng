37cc6e51a3dbc33969e8ba4d3f11bddaa7a02b80
rikimarutsui <rikimarutsui@gmail.com>
2018-07-24 22:37:38 +0800

    Gradient Descent implementation

--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -16,18 +16,21 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
+x = X(:,2);        % Get the matrix X , 2nd Column
+theta0 = theta(1); % Get vector theta, 1st value               
+theta1 = theta(2); % Get vector theta, 2nd value
+h = theta0 + (theta1 * x); % The hypothesis formula
 
+theta0 = theta0 - alpha * (1/m) * sum(h - y); % Gradient Descent for theta0
+theta1 = theta1 - alpha * (1/m) * sum((h - y) .* x); % Gradient Descent for theta1
 
-
-
-
-
+theta = [theta0; theta1]; % simultaneously update
 
     % ============================================================
 
     % Save the cost J in every iteration    
-    J_history(iter) = computeCost(X, y, theta);
-
+    J_history(iter) = computeCost(X, y, theta); % doing cost function
+    
 end
 
 end
