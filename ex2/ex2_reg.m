0d21b05e73844cece8bf3a0c7b81eaf29599d95d
rikimarutsui <rikimarutsui@gmail.com>
2018-08-02 17:36:27 +0800

    Lambda Answer

--- ex2/ex2_reg.m
+++ ex2/ex2_reg.m
@@ -107,7 +107,8 @@ pause;
 initial_theta = zeros(size(X, 2), 1);
 
 % Set regularization parameter lambda to 1 (you should vary this)
-lambda = 1;
+% Answer Lambda is 0 is the better 
+lambda = 0;
 
 % Set Options
 options = optimset('GradObj', 'on', 'MaxIter', 400);
