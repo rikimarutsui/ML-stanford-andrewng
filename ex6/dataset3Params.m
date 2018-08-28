4a94ccc0420fd33e8cfa47798e811957ae22c7b2
rikimarutsui <rikimarutsui@gmail.com>
2018-08-28 13:42:46 +0800

    Parameters (C, sigma) for Dataset 3

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -22,9 +22,46 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+C_steps     = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+sigma_steps = C_steps;
 
+error_i = [];
+error_j = [];
 
+for i = 1:size(C_steps, 1),
+  C = C_steps(i);
+  for j = 1:size(sigma_steps, 1),
+    sigma = sigma_steps(j);
+    
+    % Train the SVM model
+    model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+    predictions = svmPredict(model, Xval);
+    
+    % Compute Prediction Error
+    error = mean(double(predictions ~= yval));
+    
+    % Put error to the error j vector
+    if j == 1,
+      error_j = error;
+    else
+      error_j = [error_j; error];
+    endif
+  endfor
+  % Put error j vector to the error i matrix
+  if i == 1,
+      error_i = error_j;
+  else
+      error_i = [error_i error_j];
+  endif
+endfor
 
+% find the minimum value and index of the error_i
+[row, col] = find(error_i == min(min(error_i)));
+
+% return C and sigma
+
+C = C_steps(col);
+sigma = sigma_steps(row);
 
 
 
