6698d532636c45c63fc40d37382058eee37c3b9d
rikimarutsui <rikimarutsui@gmail.com>
2018-08-28 14:08:33 +0800

    Email Preprocessing

--- ex6/processEmail.m
+++ ex6/processEmail.m
@@ -96,8 +96,14 @@ while ~isempty(email_contents)
     % Note: You can use strcmp(str1, str2) to compare two strings (str1 and
     %       str2). It will return 1 only if the two strings are equivalent.
     %
-
-
+    for w = 1:length(vocabList),
+      if strcmp(vocabList{w}, str) == 1,
+        word_indices = [word_indices; w];
+      else
+        continue;
+      endif
+    endfor
+    
 
 
 
